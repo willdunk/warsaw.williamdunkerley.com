2bcb4629392e405edb907ee98d4a193d7c239228
William Dunkerley <hahaveryfun@gmail.com>
2020-04-07 22:18:49 -0400

    mocking with test rss file

--- app/views/review.py
+++ app/views/review.py
@@ -1,10 +1,14 @@
 from flask_restful import Resource
+import feedparser
 
 class ReviewDao():
-	def __init__(self, text):
-		self.text = text
+	def __init__(self, content=None, imageUrl=None):
+		self.content = content
+		self.imageUrl = imageUrl
 
 class Review(Resource):
 	def get(self):
-		r = ReviewDao("text")
+		x = feedparser.parse('https://letterboxd.com/hahaveryfun/rss/')
+		content = x.entries[0].description
+		r = ReviewDao(str(content), None)		
 		return r.__dict__
--- tests/views/review.py
+++ tests/views/review.py
@@ -1,21 +1,28 @@
 import pytest
 import unittest
 import json
+import feedparser
+from unittest.mock import MagicMock, patch
+import os
 
 from app import app
-from app.views.review import Review
+from app.views.review import Review, ReviewDao
+
+RSS_PATH = os.path.abspath('assets/rss/default.rss')
 
 class ReviewTest(unittest.TestCase):
 	def setUp(self): 
 		self.app = app.test_client()
 
-	def test_review_get_class(self):
-		r = Review()
-		expect = {"text": "text"}
-		self.assertDictEqual(expect, r.get())
+	@patch('feedparser.parse', return_value=feedparser.parse(RSS_PATH))
+	def test_review_get_class(self, parser_function):
+		r = ReviewDao(
+			content = feedparser.parse(RSS_PATH).entries[0].description
+		)
+		self.assertDictEqual(r.__dict__, Review().get())
 
-	def test_review_get_route(self):
+	@patch('feedparser.parse', return_value=feedparser.parse(os.path.abspath('assets/rss/default.rss')))
+	def test_review_get_route(self, parser_function):
 		expect = (Review()).get()
 		response = self.app.get('/review')
 		self.assertDictEqual(expect, response.json)
-		
