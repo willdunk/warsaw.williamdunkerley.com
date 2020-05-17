08d403293556687ac99cf53b08b04fa05b8a084b
William Dunkerley <hahaveryfun@gmail.com>
2020-05-16 20:12:08 -0400

    test

--- start.sh
+++ start.sh
@@ -36,9 +36,9 @@ docker stop ${DOCKER_IMAGE_NAME} || echo 'Cannot stop container'
 docker rm ${DOCKER_IMAGE_NAME} || echo 'Cannot remove container'
 docker stop ${DOCKER_IMAGE_NAME}.migration || echo 'Cannot stop migration container'
 docker rm ${DOCKER_IMAGE_NAME}.migration || echo 'Cannot remove migration container'
-docker build -t ${DOCKER_IMAGE_NAME}.migration . -f ./Dockerfiles/migrate.Dockerfile --build-arg CONFIG_NAME=${CONFIG_NAME} --build-arg MIGRATION_FOLDER=${MIGRATION_FOLDER}
-docker run -d --name=${DOCKER_IMAGE_NAME}.migration ${DOCKER_IMAGE_NAME}.migration
-docker wait ${DOCKER_IMAGE_NAME}.migration
+# docker build -t ${DOCKER_IMAGE_NAME}.migration . -f ./Dockerfiles/migrate.Dockerfile --build-arg CONFIG_NAME=${CONFIG_NAME} --build-arg MIGRATION_FOLDER=${MIGRATION_FOLDER}
+# docker run -d --name=${DOCKER_IMAGE_NAME}.migration ${DOCKER_IMAGE_NAME}.migration
+# docker wait ${DOCKER_IMAGE_NAME}.migration
 docker rm ${DOCKER_IMAGE_NAME}.migration || echo 'Cannot remove migration container'
 # docker build -t ${DOCKER_IMAGE_NAME} . -f ./Dockerfiles/Dockerfile --build-arg CONFIG_NAME=${CONFIG_NAME}
 # docker run -d -p ${DOCKER_CONTAINER_PORT}:5000 --name=${DOCKER_IMAGE_NAME}
