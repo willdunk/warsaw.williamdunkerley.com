68ce4af38af2a7c3525d28ee3abecbcf271864b0
William Dunkerley <hahaveryfun@gmail.com>
2020-12-13 18:07:32 -0600

    feat(review): Add pagination (& sorting) to review query

--- app/resource/review.py
+++ app/resource/review.py
@@ -1,16 +1,23 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, reqparse
 from app.service import Review as ReviewService
 from app.app import api
 from app.utils import review_fields
 
 api = Namespace('review', description='Review operations')
 
+parser = reqparse.RequestParser()
+parser.add_argument('page_number', location="args", type=int)
+parser.add_argument('page_size', location="args", type=int)
+parser.add_argument('order_by', location="args", choices=('title', 'published_date', 'watched_date'), default='published_date')
+parser.add_argument('order_direction', location="args", choices=('ASC', 'DESC'), default='ASC')
+
 @api.route('')
 class Reviews(Resource):
 	@api.doc(security=None)
+	@api.expect(parser)
 	@api.marshal_with(review_fields)
 	def get(self):
-		return ReviewService().getReviews()
+		return ReviewService().getReviews(**parser.parse_args())
 
 @api.route('/<string:review_uuid>')
 class Review(Resource):
--- app/service/review.py
+++ app/service/review.py
@@ -3,8 +3,21 @@ from typing import List
 from app.model import ReviewModel
 
 class Review():
-	def getReviews(self) -> List[ReviewModel]:
-		return ReviewModel.query.order_by(ReviewModel.title).all()
+	def getReviews(self, page_number=None, page_size=None, order_by='published_date', order_direction='DESC') -> List[ReviewModel]:
+		print(page_number)
+		print(page_size)
+		print(order_by)
+		print(order_direction)
+		column = {
+			'title': ReviewModel.title,
+			'published_date': ReviewModel.published_date,
+			'watched_date': ReviewModel.watched_date
+		}[order_by]
+		column_ordered = {
+			'ASC': column.asc(),
+			'DESC': column.desc()
+		}[order_direction]
+		return ReviewModel.query.order_by(column_ordered).paginate(page_number, page_size, True).items
 
 	def getReview(self, index) -> ReviewModel:
 		return ReviewModel.query.filter_by(review_id=index).first()
