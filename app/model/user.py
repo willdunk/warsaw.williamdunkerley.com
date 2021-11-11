f5a377a540f0d6f53fd9d7ca6df0b0f5614ead59
William Dunkerley <hahaveryfun@gmail.com>
2021-11-10 21:16:44 -0500

    fix(*)

--- app/model/user.py
+++ app/model/user.py
@@ -15,6 +15,6 @@ class RevokedTokenModel(BaseModel, db.Model):
 	jti = db.Column(db.String(120))
 
 @jwt.token_in_blocklist_loader
-def check_if_token_in_blocklist(jwt_headers, jwt_payload):
+def check_if_token_in_blocklist(jwt_header, jwt_payload):
 	jti = jwt_payload['jti']
 	return bool(RevokedTokenModel.query.filter_by(jti=jti).first())
